4da27fb5bf0a92931a4dbe73fbc5d604377e0183
akjw <aseykoh@gmail.com>
2020-12-21 18:01:27 +0800

    day1

---
+++ 01/day1.hs
@@ -0,0 +1,19 @@
+find2020Pair :: [Int] -> Int
+find2020Pair nums = head [x*y | x <- nums, y <- nums, x + y == 2020]
+
+find2020Triple :: [Int] -> Int
+find2020Triple nums = head [x*y*z | x <- nums, y <- nums, z <- nums, x + y + z == 2020]
+
+
+rInt :: String -> Int
+rInt = read
+
+main :: IO ()
+main = do 
+   input <- readFile "input.txt"
+   putStrLn "Part 1:"
+   print $ find2020Pair $ map (read :: String -> Int) . lines $ input
+   putStrLn "Part 2:"
+   print $ find2020Triple $ map (read :: String -> Int) . lines $ input
+  
+  
