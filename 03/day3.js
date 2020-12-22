efbe7cc94c2875dd5d85f4f8d9e82418bec78c2c
akjw <aseykoh@gmail.com>
2020-12-22 17:55:30 +0800

    add day3

--- 03/day3.js
+++ 03/day3.js
@@ -5,19 +5,31 @@ fs.readFile('./input.txt', 'utf8', function(err, data){
     throw err;
   }
     const lines = data.split("\n");
-    const map = lines.map((line) => line.split(''))
-    console.log(map[0][0])
-    let trees = 0;
-    let row = 0;
-    let col = 0;
+    const map = lines.map((line) => line.split(''));
+    const increments = [[1,1], [3,1], [5,1], [7,1], [1,2]];
+    let treesList = [];
+  
     
-    while (row < map.length) {
-      if (map[row][col] == '#') { 
-        trees++;
-      }
-
-      col = (col + 3) % map[0].length;
-      row += 1;
+    for(let i = 0; i < increments.length; i++){
+      let increment = increments[i];
+      let trees = countTrees(map, increment[0], increment[1]);
+      treesList.push(trees);
     }
-    console.log(trees)
+    const multiplyTrees = treesList.reduce((a, b)=> a*b, 1);
+    console.log(`PART 1: Total number of trees is ${treesList[1]}`)
+    console.log(`PART 2: Product of all trees is ${multiplyTrees}`)
 }); 
+
+function countTrees(map, colIncrement, rowIncrement){
+  let row = 0;
+  let col = 0;
+  let trees = 0;
+  while (row < map.length) {
+    if (map[row][col] == '#') { 
+      trees++;
+    }
+    col = (col + colIncrement) % map[0].length;
+    row += rowIncrement;
+  }
+  return trees
+}
